747e6c8020af39cfee7f5bf6741f722d5b2c6b91
yhnkaya <ayhanprofessional@gmail.com>
2023-01-12 14:07:38 -0500

    init

--- flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -1,10 +1,19 @@
+
+
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html", name="Elyes", lastname= "Ferjani")
+
+
 @app.route("/body/<int:x>/<int:y>")
 def body(x, y):
     return render_template("body.html", x=x, y=y, sum = x+y)
+
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3001, debug=False)
